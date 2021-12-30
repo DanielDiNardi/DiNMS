58b8588a2d161f6e654d24401d61a6f0bbe5f160
Daniel Di Nardi <c18487682@mytudublin.ie>
2021-12-30 14:02:46 +0100

    test html page

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const db = new sqlite3.Database('');
 var app = express();
 const port = process.env.port || 3000;
 
+app.get('/',function(req,res) {
+  res.sendFile('test.html');
+});
+
 app.listen(port, (err) => {
     if (err) {
       return console.log("ERROR: " + err);
