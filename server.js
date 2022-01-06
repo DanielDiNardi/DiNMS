2a330959ff4e6323873534fa32fc07db17bf33ac
Daniel Di Nardi <c18487682@mytudublin.ie>
2022-01-06 22:47:32 +0100

    Created EJS layout and started Login and Registration page

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('');
 var app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/',function(req,res) {
-  res.sendFile(__dirname + '/test.html');
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "public/css"));
+app.use("/js", express.static(__dirname + "public/js"));
+app.use("/img", express.static(__dirname + "public/img"));
+app.use(expressLayouts);
+
+app.set("view engine", "ejs");
+
+app.get('',function(req, res) {
+  res.render("login");
+});
+
+app.get('/registration',function(req, res) {
+  res.render("registration");
 });
 
 app.listen(port, (err) => {
