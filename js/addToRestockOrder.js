9d3fce408006ba0976b2e8e7c9bbfe1f03346ac9
Daniel Di Nardi <c18487682@mytudublin.ie>
2022-03-21 10:44:10 +0000

    Got restock order array to be stored in local storage and found HUGE bug with stock not being deducted by items but by order (gotta fix that)

--- js/addToRestockOrder.js
+++ js/addToRestockOrder.js
@@ -1,14 +1,15 @@
+var restock_array = [];
+
 function addToRestockOrder(){
     getStock("/get-stock")
     .then(
         function(req, res){
             req.forEach(function(stock_item){
-                console.log(
-                    "Curr: " + stock_item.current_stock + "\n" +
-                    "Min: " + stock_item.restock_min
-                );
-                if(stock_item.current_stock < stock_item.restock_min){
-                    console.log(stock_item);
+
+                if(stock_item.current_stock < stock_item.restock_min && localStorage.getItem("RestockOrder").split(",").indexOf(stock_item.id) == -1){
+                    restock_array.push(stock_item.id);
+                    localStorage.setItem("RestockOrder", restock_array.toString());
+                    console.log(localStorage.getItem("RestockOrder"));
                 }
             });
         }
