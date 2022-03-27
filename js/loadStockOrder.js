deb1db463ceec1ae1583a9ec9bdc721b79bc0122
Daniel Di Nardi <c18487682@mytudublin.ie>
2022-03-27 20:13:57 +0100

    Fixed restock order display and restock order can be downloaded as csv

--- js/loadStockOrder.js
+++ js/loadStockOrder.js
@@ -1,18 +1,33 @@
 function addStockOrderToDisplay(){
+    
+    var rows = [];
+
     getStock("/get-stock")
     .then(
         function(req, res){
             req.forEach(function(stock_item){
-                localStorage.getItem("RestockOrder").split(",").forEach(function(id){
+                localStorage.getItem("RestockOrder").split(", ").forEach(function(id){
 
                     if(stock_item.id === id){
+
+                        rows.push([
+                            stock_item.name, 
+                            parseFloat(parseFloat(stock_item.restock_max).toFixed(2) - parseFloat(stock_item.current_stock).toFixed(2)).toFixed(2),
+                            stock_item.unit
+                        ]);
+                        
                         document.getElementsByClassName("stock_order_display")[0].innerHTML += `
-                            <div>${stock_item.name}</div>
-                            <div>${parseFloat(stock_item.restock_max).toFixed(2) - parseFloat(stock_item.current_stock).toFixed(2)}</div>
-                        `
+                            <div class="stock_order_item">
+                            ${stock_item.name} 
+                            ${parseFloat(parseFloat(stock_item.restock_max).toFixed(2) - parseFloat(stock_item.current_stock).toFixed(2)).toFixed(2)} 
+                            ${stock_item.unit}
+                            </div>
+                        `;
                     }
                 });
             });
+
+            localStorage.setItem("Rows", rows.toString());
         }
     );
 }
--- js/printStockOrder.js
+++ js/printStockOrder.js
@@ -1,4 +1,45 @@
 function printStockOrder(){
+
+    var csvContent = "data:text/csv;charset=utf-8,";
+
+    reconstructStockOrder().forEach(function(rowArray) {
+
+        let row = rowArray.join(",");
+        csvContent += row + "\r\n";
+    });
+
+    var encodedUri = encodeURI(csvContent);
+    var now = new Date();
+
+
+    var hiddenElement = document.createElement('a');  
+    hiddenElement.href = encodedUri;  
+    hiddenElement.target = '_blank';  
+      
+    hiddenElement.download = 'StockOrder-' + now +'.csv';  
+    hiddenElement.click();  
+
+
+    localStorage.setItem("Rows", "");
     localStorage.setItem("RestockOrder", "");
     document.getElementsByClassName("stock_order_display")[0].innerHTML = "";
+}
+
+function reconstructStockOrder(){
+    var protoStockOrder = localStorage.getItem("Rows").split(",");
+    var csv = [];
+    var row = [];
+
+    protoStockOrder.push("filler");
+
+    protoStockOrder.forEach(function(item, i){
+
+        if(i % 3 == 0 && i !== 0){
+            csv.push(row);
+            row = [];
+        }
+        row.push(item);
+    });
+
+    return csv;
 }
